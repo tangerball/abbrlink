a205b2bad2493156c15f09aa994ced5927051e40
林华燕 <514936598@qq.com>
2026-04-09 23:39:25 +0800

    feat: extend AbbrLinkConfig interface with new options

--- packages/cli/src/model/abbrLink.ts
+++ packages/cli/src/model/abbrLink.ts
@@ -3,10 +3,30 @@
  */
 export interface AbbrLinkConfig {
   /**
-   * @description:  Algorithm
+   * @description:  Algorithm used to generate abbrlink
    * @default 'crc32'
    */
   alg: 'crc32' | 'crc16'
+
+  /**
+   * @description:  Representation format of abbrlink
+   * @default 'hex'
+   * @description 'hex' for hexadecimal (e.g., a1b2c3d4), 'dec' for decimal (e.g., 123456789)
+   */
+  rep?: 'hex' | 'dec'
+
+  /**
+   * @description:  Time zone offset in hours
+   * @default 8
+   */
+  timeOffsetInHours?: number
+
+  /**
+   * @description:  Custom field name in front matter
+   * @default 'abbrlink'
+   * @description Allows using a custom field name instead of 'abbrlink' in the front matter
+   */
+  fieldName?: string
 }
 
 export interface FrontMatter {
